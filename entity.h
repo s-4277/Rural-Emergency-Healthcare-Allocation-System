b2ac80dc6ef6a3f2742206d0f76ebff54fe25c29
Subham Roshan <subhamroshan051@gmail.com>
2026-05-06 08:16:42 +0530

    Update entity.h

--- entity.h
+++ entity.h
@@ -1,1 +1,30 @@
 
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Entity {
+protected:
+    string id;
+    string name;
+
+public:
+    Entity(const string& id, const string& name)
+        : id(id), name(name) {}
+
+    // virtual destructor
+    virtual ~Entity() {}
+
+    // pure virtual functions
+    virtual void display() const = 0;
+    virtual string getID() const = 0;
+
+    string getName() const {
+        return name;
+    }
+};
+
+#endif 
